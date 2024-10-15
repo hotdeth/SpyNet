0003d329e48406f15e1124986f46c97b2e84dc6b
Ridha Karem <radhiox4@icloud.com>
2024-10-15 19:47:46 +0300

    Work On Discover

--- SpyNet.py
+++ SpyNet.py
@@ -1,27 +1,30 @@
 from colorama import Fore, Back, Style
 import os
+import nmap
 
 #This function to clear the screen
 def Clear_Screen():
     os.system("cls")
 
+nocolor = "\033[0;37;10m"
+azul = "\033[0;34;10m"
+amarillo = "\033[1;33;10m"
 
 #Main Menu class with the Banar and the choice of the first begin
 class MainMenu:
     def Baner(self):
-        baner = (Fore.RED+ r"""   _____             _   __     __ 
+        baner = (azul + r"""   _____             _   __     __ 
   / ___/____  __  __/ | / /__  / /_
   \__ \/ __ \/ / / /  |/ / _ \/ __/
  ___/ / /_/ / /_/ / /|  /  __/ /_  
 /____/ .___/\__, /_/ |_/\___/\__/  
     /_/    /____/                  """)
         print(baner)
-        print(Style.RESET_ALL)
         
         print( "****************************************")
 
     def Explain(self):
-        print(r"""THIS PROJECT UNDER  *GNU LICENSE*
+        print(amarillo + r"""THIS PROJECT UNDER  *GNU LICENSE*
 IF THERE IS ANY ISSUE PLEASE CONTENT WITH US ON:
 spynet4sc@gmail.com
 READ MORE ON https://github.com/hotdeth/SpyNet 
@@ -30,13 +33,13 @@ READ MORE ON https://github.com/hotdeth/SpyNet
      
 #Ask the user to Run or exit or show the requirements
     def choice(self):
-        UserChoice = input("""1-Run program
+        UserChoice = input(nocolor + """1-Run program
 2-Show the requirement 
 3-Exit
 Input:""")
         return UserChoice
     def choice2(self):
-        choice2 = input("""1-Port checkout
+        choice2 = input(nocolor + """1-Port checkout
 2-Network Discover
 Input:""")
         return choice2
@@ -51,9 +54,24 @@ Input:""")
 
 
 class Discover:
-    pass
+    def __init__(self):
+        ip = input("IP address Deafault is (192.168.1.1/192.168.0.1):")
+        self.ip = ip
 
+    
+    def NetworkScan(self):
+        if len(self.ip) == 0:
+            network = '192.168.1.1/24'
+        else:
+            network = self.ip + '/24'
 
+        print("Scanning Please Wait!")
+
+        nm = nmap.PortScanner()
+        nm.scan(hosts=network,arguments='-sn')
+        host_list = [(x,nm[x]['status']['state']) for x in nm.all_hosts()]
+        for host,status in host_list:
+            print(f"Host {host}")
 
 
 class Port:
@@ -115,15 +133,6 @@ class Run:
         print(Fore.GREEN + "See you late")
         print(Style.RESET_ALL)
     
-
-
-
-
-user = Run()
-user.Start()
-
-
-
-
-
+User = Run()
+User.Start()
 
